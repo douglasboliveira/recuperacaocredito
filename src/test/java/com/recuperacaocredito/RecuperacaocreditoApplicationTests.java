8c5889a0bfb153f70fedf8a4f6d134b92f8e122a
Your Name <you@example.com>
2021-03-15 13:30:22 -0300

    initial commit

---
+++ src/test/java/com/recuperacaocredito/RecuperacaocreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recuperacaocredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuperacaocreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
